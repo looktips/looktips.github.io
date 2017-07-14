784e028c5b49851f14faca0d00faaff1b8637040
writer <writermail>
2017-07-14 19:39:56 +0800

    first post

---
+++ files/js/basis.js
@@ -0,0 +1,4 @@
+function subscribe() {
+	var boxtem = '<div id="subscribemain" class="mainbody"><p>如何便捷访问本站：</p><ul><li>1. 点击浏览器“发送到”<p>iOS在浏览器下方第三个按钮;</p><p>Android通常在浏览器右上方的“...”按钮;</p></li><li>2. 选择“添加到主屏幕”<p>点击手机主屏幕新增的这个图标即可便捷访问本站;</p><p>注：本站属在线访问请留意联网方式避免流量过度使用;</p></li></ul><p class="close">关闭</p></div>';
+	document.getElementById("subscribebox").innerHTML = boxtem;
+}
